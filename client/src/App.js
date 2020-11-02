19aa8f9f985da10005fe8906de4feef42d41ece0
shubham-kumar02 <krshubham878@gmail.com>
2020-11-03 00:57:35 +0530

    added login-signup

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Switch, Route, NavLink } from "react-router-dom";
 
-
+import Login from "./components/auth/login.js";
+import SignUp from "./components/auth/signup.js";
+import Home from "./components/home/home.js";
+import Dashboard from "./components/dashboard/dashboard.js"
 function App() {
   return (
+  <Router>
     <div className="App">
-      <h1>Hello world</h1>
+      <div>
+          <Switch>
+            <Route exact path='/' component={Login} />
+            <Route exact path="/sign-in" component={Login} />
+            <Route exact path="/sign-up" component={SignUp} />
+            <Route exact path="/home" component={Home}/>
+            <Route exact path="/dashboard" component={Dashboard}/>
+          </Switch>
+      </div>
+      
     </div>
+  </Router>
   );
 }
 
--- client/src/components/home/home.js
+++ client/src/components/home/home.js
@@ -1,1 +1,11 @@
-//hello world
+import React from 'react';
+import Navbar from './navbar';
+const Home = () => {
+    return(
+        <>
+        <Navbar/>
+        <h1>home</h1>
+        </>
+    );
+}
+export default Home;
